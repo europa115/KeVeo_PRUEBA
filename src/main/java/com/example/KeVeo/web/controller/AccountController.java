1669517e9dbaaff972df1ab63e66d90bc699aa39
Esteban Torres <estebanto92@gmail.com>
2022-08-10 18:59:12 +0200

    Creando ACCOUNT **Añadido cambiar contraseña** erros con la confirmacion de pass. la primera vez no funciona, se podria arreglar pasando la logica del javaScrip al controller.
    ------------------------------------
    falta añadir favicoin, cambiar el nombre de arriba por el icono.
    
    plantearse cambiar el color del boton de encoger.
    
    corregir warnings html registro
    
    Faltaria arreglar un poco el registro de la imagen, enseñar y dar opciones.
    
    En el editar usuario, poder ver la imagen y editarla.
    
    con respcto a imagenes preguntar si es mejor tenerlo como string o almacenar como bytes.

--- src/main/java/com/example/KeVeo/web/controller/AccountController.java
+++ src/main/java/com/example/KeVeo/web/controller/AccountController.java
@@ -1,38 +1,46 @@
 package com.example.KeVeo.web.controller;
 
 
+import com.example.KeVeo.data.entity.User;
 import com.example.KeVeo.data.repository.UserRepository;
 import com.example.KeVeo.dto.UserDTO;
 import com.example.KeVeo.service.MenuService;
 import com.example.KeVeo.service.UserService;
 import com.example.KeVeo.service.mapper.UserMapper;
+
+
+import org.aspectj.bridge.Message;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpSession;
 
 
 @Controller
-public class AccountController extends AbstractController<UserDTO>{
+public class AccountController extends AbstractController<UserDTO> {
     private UserMapper userMapper;
     private UserService userService;
     private UserRepository userRepository;
+
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Autowired
-    protected AccountController(MenuService menuService, UserMapper userMapper, UserService userService, UserRepository userRepository) {
+    protected AccountController(MenuService menuService, UserMapper userMapper, UserService userService, UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         super(menuService);
-        this.userMapper=userMapper;
-        this.userService=userService;
-        this.userRepository=userRepository;
+        this.userMapper = userMapper;
+        this.userService = userService;
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @GetMapping("/account")
@@ -40,20 +48,20 @@ public class AccountController extends AbstractController<UserDTO>{
         SecurityContext context = SecurityContextHolder.getContext();
         Authentication authentication = context.getAuthentication();
 
-        if(authentication==null){
+        if (authentication == null) {
 
             return "/login";
 
-        }else return "account/account-info";
+        } else return "account/account-info";
 
-        //*********Puede que esto no funcione, revisar cuando este rol anonimo instaurado****************
 
     }
-    @PostMapping({ "/{id}/account" })
-    public Object changePhoto(@PathVariable(value = "id") Integer id, SessionStatus status,@RequestParam("file") MultipartFile photo) {
+
+    @PostMapping({"/{id}/account"})
+    public Object changePhoto(@PathVariable(value = "id") Integer id, SessionStatus status, @RequestParam("file") MultipartFile photo) {
         try {
-            UserDTO userDTO= this.userService.findById(id).get();
-            userService.changePhoto(photo,userDTO);
+            UserDTO userDTO = this.userService.findById(id).get();
+            userService.changePhoto(photo, userDTO);
 
         } catch (DataIntegrityViolationException exception) {
             status.setComplete();
@@ -68,10 +76,37 @@ public class AccountController extends AbstractController<UserDTO>{
     }
 
     @GetMapping("/{id}/account/changePassword")
-    public String changePassword(@PathVariable(value = "id") Integer id){
+    public String viewChangePassword(@PathVariable(value = "id") Integer id) {
 
 
         return "account/changePassword";
     }
+
+    @PostMapping("/changePassword")
+    public String changePassword(@RequestParam("oldPassword") String oldPassword, @RequestParam("newPassword") String newPassword, Model model) {
+
+        UserDTO userDTO = userService.findById(((User) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getId()).get();
+
+        if (bCryptPasswordEncoder.matches(oldPassword, userDTO.getPassword())) {
+
+            userDTO.setPassword(bCryptPasswordEncoder.encode(newPassword));
+
+            this.userRepository.save(userMapper.toEntity(userDTO));
+
+            model.addAttribute("successful", true);
+
+        } else {
+
+            String message = "Contraseña incorrecta";
+
+            model.addAttribute("errors", true);
+
+            return "/account/changePassword";
+        }
+
+        return "account/changePassword";
+
+    }
+
 
 }
