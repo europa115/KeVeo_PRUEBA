1d6a0fe469b3d7f1505bbf13c4439f8a2ef601d8
Esteban Torres <estebanto92@gmail.com>
2022-08-14 19:54:26 +0200

    Arreglando un poco la vista movil **Creando Pagina peliculas.**
    Arreglar buscador e introducir genero, y plataformas.
    
    Falta establecer la entidad comentarios.
    
    Lo suyo seria establecer un Scrip para que se creara toda la base de datos.
    
    ------------------------------------
    falta añadir favicoin, cambiar el nombre de arriba por el icono.
    
    En el apartado cuenta crear un html para borrar la cuenta.
    
    plantearse cambiar el color del boton de encoger.
    
    corregir warnings html registro
    
    Faltaria arreglar un poco el registro de la imagen, enseñar y dar opciones.
    
    En el editar usuario, poder ver la imagen y editarla.
    
    tal vez añadir en gestion algo para ver usuarios y crearlos
    
    con respcto a imagenes preguntar si es mejor tenerlo como string o almacenar como bytes.

--- src/main/resources/static/js/layoutScript.js
+++ src/main/resources/static/js/layoutScript.js
@@ -1,3 +1,9 @@
-$('.btn-expand-collapse').click(function(e) {
-				$('.navbar-primary').toggleClass('collapsed');
-});
+if (window.matchMedia("(min-width: 400px)").matches) {
+  $('.btn-expand-collapse').click(function(e) {
+    				$('.navbar-primary').toggleClass('collapsed');
+    });
+} else {
+
+
+  $('.navbar-primary').toggleClass('collapsed');
+}
