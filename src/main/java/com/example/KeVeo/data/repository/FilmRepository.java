dce40d6bd7958f5a87b41ce67c9be1d21065cdb2
Esteban Torres <estebanto92@gmail.com>
2022-08-15 20:02:14 +0200

    **Crear visual Generos y plataformas**
    Falta establecer la entidad comentarios.
    
    Lo suyo seria establecer un Scrip para que se creara toda la base de datos.
    
    ------------------------------------
    falta añadir favicoin, cambiar el nombre de arriba por el icono.
    
    En el apartado cuenta crear un html para borrar la cuenta.
    
    plantearse cambiar el color del boton de encoger.
    
    corregir warnings html registro
    
    Faltaria arreglar un poco el registro de la imagen, enseñar y dar opciones.
    
    En el editar usuario, poder ver la imagen y editarla.
    
    tal vez añadir en gestion algo para ver usuarios y crearlos
    
    con respcto a imagenes preguntar si es mejor tenerlo como string o almacenar como bytes.

--- src/main/java/com/example/KeVeo/data/repository/FilmRepository.java
+++ src/main/java/com/example/KeVeo/data/repository/FilmRepository.java
@@ -11,10 +11,11 @@ import java.util.List;
 
 @Repository
 public interface FilmRepository extends JpaRepository<Film, Integer> {
-    @Query("SELECT u FROM User u WHERE"
-            + " CONCAT(u.id, u.userName, u.userEmail)"
-            + " LIKE %?1%")
-    Page<Film> findAll(Pageable pageable, String wordKey);
 
+    @Query("SELECT distinct f FROM Film f  JOIN f.genres g JOIN f.platforms p WHERE"
+            +  " CONCAT(f.title,g.name,f.casting,p.name)"
+            + " LIKE %?1%"
+    )
+    Page<Film> findAll(Pageable pageable, String wordKey);
 
 }
