794a29a5e4ca6cfdd9d863e3abd8fcd4278faace
Esteban Torres <estebanto92@gmail.com>
2022-08-17 00:32:15 +0200

    **Creando Info Peliculas**
    Falta establecer la entidad comentarios.
    
    Lo suyo seria establecer un Scrip para que se creara toda la base de datos.
    
    Añadir mas opciones a la classe pelicula(ultimosEstrenos, mas populares....)
    ------------------------------------
    falta añadir favicoin, cambiar el nombre de arriba por el icono.
    
    En el apartado cuenta crear un html para borrar la cuenta.
    
    plantearse cambiar el color del boton de encoger.
    
    corregir warnings html registro
    
    Faltaria arreglar un poco el registro de la imagen, enseñar y dar opciones.
    
    En el editar usuario, poder ver la imagen y editarla.
    
    tal vez añadir en gestion algo para ver usuarios y crearlos
    
    con respcto a imagenes preguntar si es mejor tenerlo como string o almacenar como bytes.

--- src/main/java/com/example/KeVeo/web/controller/FilmController.java
+++ src/main/java/com/example/KeVeo/web/controller/FilmController.java
@@ -1,21 +1,30 @@
 package com.example.KeVeo.web.controller;
 
+import com.example.KeVeo.data.entity.Film;
 import com.example.KeVeo.data.entity.Genre;
+import com.example.KeVeo.data.entity.Role;
 import com.example.KeVeo.data.entity.User;
 import com.example.KeVeo.data.repository.GenreRepository;
 import com.example.KeVeo.dto.FilmDTO;
 import com.example.KeVeo.dto.UserDTO;
 import com.example.KeVeo.service.FilmService;
 import com.example.KeVeo.service.MenuService;
+import com.example.KeVeo.service.mapper.FilmMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 import java.util.Optional;
@@ -26,12 +35,15 @@ public class FilmController extends AbstractController<FilmDTO> {
 
     private GenreRepository genreRepository;
 
+    private FilmMapper filmMapper;
+
     @Autowired
-    protected FilmController(MenuService menuService, FilmService filmService,GenreRepository genreRepository) {
+    protected FilmController(MenuService menuService, FilmService filmService,GenreRepository genreRepository,FilmMapper filmMapper) {
         super(menuService);
 
         this.filmService = filmService;
         this.genreRepository=genreRepository;
+        this.filmMapper=filmMapper;
     }
 
     @GetMapping("/film")
@@ -47,4 +59,15 @@ public class FilmController extends AbstractController<FilmDTO> {
                 .addAttribute(pageNumbersAttributeKey, getPageNumbers(listFilms));
         return "film/filmAll";
     }
+
+    @GetMapping("/film/filmInfo/{id}")
+    public String viewInfo(@PathVariable("id") Integer id, ModelMap model) {
+        FilmDTO filmDTO = filmService.findById(id).get();
+        Film film= filmMapper.toEntity(filmDTO);
+
+        model.addAttribute("film", film);
+
+        return "film/filmInfo";
+    }
+
 }
