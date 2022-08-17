f111bc2588ed9f6093493e784a3e88561b95769b
Esteban Torres <estebanto92@gmail.com>
2022-08-18 01:10:12 +0200

    **Crear crud peliculas**
    Falta establecer la entidad comentarios.
    
    Lo suyo seria establecer un Scrip para que se creara toda la base de datos.
    
    Añadir mas opciones a la classe pelicula(ultimosEstrenos, mas populares....)
    ------------------------------------
    falta añadir favicoin, cambiar el nombre de arriba por el icono.
    
    En el apartado cuenta crear un html para borrar la cuenta.
    
    plantearse cambiar el color del boton de encoger.
    
    corregir warnings html registro
    
    Faltaria arreglar un poco el registro de la imagen, enseñar y dar opciones.
    
    En el editar usuario, poder ver la imagen y editarla.
    
    tal vez añadir en gestion algo para ver usuarios y crearlos
    
    con respcto a imagenes preguntar si es mejor tenerlo como string o almacenar como bytes.

--- src/main/resources/static/js/layoutScript.js
+++
@@ -1,9 +0,0 @@
-if (window.matchMedia("(min-width: 400px)").matches) {
-  $('.btn-expand-collapse').click(function(e) {
-    				$('.navbar-primary').toggleClass('collapsed');
-    });
-} else {
-
-
-  $('.navbar-primary').toggleClass('collapsed');
-}
