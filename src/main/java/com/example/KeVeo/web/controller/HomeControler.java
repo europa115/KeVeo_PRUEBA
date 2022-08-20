0dd727a6be27cbd9348b00831c70e20475681c8f
Esteban Torres <estebanto92@gmail.com>
2022-08-20 21:17:48 +0200

    **Creando Home*****
    Populares  terminadas
    
    Falta establecer la entidad comentarios.
    
    Lo suyo seria establecer un Scrip para que se creara toda la base de datos.
    
    Añadir mas opciones a la classe pelicula(ultimosEstrenos, mas populares....)
    
    ------------------------------------
    falta añadir favicoin, cambiar el nombre de arriba por el icono.
    
    En el apartado cuenta crear un html para borrar la cuenta.
    
    gestionPeliculas hacer que las imagenes y videos sean links en vez de texto.
    
    plantearse cambiar el color del boton de encoger.
    
    corregir warnings html registro
    
    Faltaria arreglar un poco el registro de la imagen, enseñar y dar opciones.
    
    En el editar usuario, poder ver la imagen y editarla.
    
    tal vez añadir en gestion algo para ver usuarios y crearlos
    
    con respcto a imagenes preguntar si es mejor tenerlo como string o almacenar como bytes.

--- src/main/java/com/example/KeVeo/web/controller/HomeControler.java
+++ src/main/java/com/example/KeVeo/web/controller/HomeControler.java
@@ -1,20 +1,41 @@
 package com.example.KeVeo.web.controller;
 
+import com.example.KeVeo.data.entity.Genre;
+import com.example.KeVeo.dto.FilmDTO;
+import com.example.KeVeo.service.FilmService;
 import com.example.KeVeo.service.MenuService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class HomeControler extends AbstractController{
 
+    private FilmService filmService;
+
     @Autowired
-    protected HomeControler(MenuService menuService) {
+    protected HomeControler(MenuService menuService, FilmService filmService) {
         super(menuService);
+        this.filmService=filmService;
     }
 
     @GetMapping("/")
-    public String viewHomePage() {
+    public String viewHomePage(Model model) {
+
+            final List<Genre> listGenres=filmService.listGenres();
+            final List<FilmDTO> listFilms = this.filmService.findAll();
+            model
+                    .addAttribute("listFilms", listFilms)
+                    .addAttribute("listGenres", listGenres);
+
+
         return "home";
     }
 
